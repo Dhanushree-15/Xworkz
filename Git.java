8ab5ba3eb17cf9c498009ac6a56b644b261f6ffc
Dhanushree-15 <dhanushree.1516@gmail.com>
2025-03-08 11:50:36 +0530

    its java simple code

---
+++ Git.java
@@ -0,0 +1,10 @@
+class Git{
+public static void main(String[] args){
+
+System.out.println("git clones repository url");
+System.out.println(" url");
+System.out.println(" repository url");
+}
+
+
+}
