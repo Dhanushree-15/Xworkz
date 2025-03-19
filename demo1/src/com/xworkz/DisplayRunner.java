293152cd2d555d8202bfc39ae04a754c6a09be10
Dhanushree-15 <dhanushree.1516@gmail.com>
2025-03-19 09:02:21 +0530

    commiting new

---
+++ demo1/src/com/xworkz/DisplayRunner.java
@@ -0,0 +1,13 @@
+package com.xworkz;
+
+public class DisplayRunner {
+    public static void main(String[] args) {
+        Shop shop = new Shop();
+        Display display = new Display();
+
+        shop.ads(display); // Valid case
+        shop.ads(null); // Null case
+    }
+}
+
+
---
+++ demo1/src/com/xworkz/Rocket/Rocket.java
@@ -0,0 +1,4 @@
+package com.xworkz.Rocket;
+
+public class Rocket {
+}
